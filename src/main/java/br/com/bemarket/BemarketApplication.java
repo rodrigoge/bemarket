ed0f2202856f4b9406e234ec974003196189a0b8
rodrigoge <rgestevao@gmail.com>
2022-12-05 21:13:44 -0300

    start: initializing project and restructuring folders

---
+++ src/main/java/br/com/bemarket/BemarketApplication.java
@@ -0,0 +1,13 @@
+package br.com.bemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BemarketApplication.class, args);
+	}
+
+}
